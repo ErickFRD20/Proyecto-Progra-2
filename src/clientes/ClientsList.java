f0f947a5bc368b150c51610dfdacb1964e94d35a
erickfrd <erreyesde@est.utn.ac.cr>
2026-08-26 17:54:56 -0600

    Se crea ClientsLists que permite buscar un cliente por la id y se obtiene la lista completa

--- src/clientes/ClientsList.java
+++ src/clientes/ClientsList.java
@@ -3,11 +3,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package clientes;
-
+import java.util.ArrayList;
 /**
  *
  * @author erick
  */
 public class ClientsList {
-    
+    private ArrayList<Cliente> clientes;
+    public ClientsList(){
+        clientes = new ArrayList<>();
+    }
+    public Cliente buscarId(String id){
+        for (Cliente cliente : clientes){
+            if (cliente.getId().equals(id)) {
+                return cliente;
+            }
+        }
+        return null;
+    }
+    public ArrayList<Cliente> getClientes(){
+        return clientes;
+    }
 }
