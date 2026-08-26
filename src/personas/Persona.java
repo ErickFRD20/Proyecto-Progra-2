2e18053653bc41f860f46c71f13fc457f81b8e24
erickfrd <erreyesde@est.utn.ac.cr>
2026-08-25 20:29:02 -0600

    Se programa persona con sus atributos y respectivos metodos

--- src/personas/Persona.java
+++ src/personas/Persona.java
@@ -9,5 +9,35 @@ package personas;
  * @author erick
  */
 public class Persona {
+    private String id;
+    private String nombre;
+    private String telefono;
+    
+    public Persona(String id, String nombre, String telefono){
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
     
 }
