00dd8513a0119415c38ba932eb1c98f28b2e0a61
Daniel-Delgado <josedanieldelgadoquiros@gmail.com>
2026-08-27 11:29:59 -0600

    clase servivios extrao adicionales
    aqui esta programada la clase con sus metodos get y set y con el nombre de sus variables ya definidas

---
+++ src/servicios/servicioExtra.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package servicios;
+
+/**
+ *
+ * @author josed
+ */
+public class servicioExtra {
+    private int codigo;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public servicioExtra(int codigo, String nombre, String descripcion, double precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+    
+}
