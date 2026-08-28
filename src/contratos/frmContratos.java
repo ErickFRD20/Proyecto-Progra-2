66a9553f709d789343bb441357b49aca281281d9
Marco Ramirez Quiros <maaramirezqu@est.utn.ac.cr>
2026-08-28 01:23:16 -0600

    Se crea al fin el formulario de contratos
    Ya supuestamente todo esta bien en el frm de contratos quedo feo pero se hizo lo que se pudo

--- src/contratos/frmContratos.java
+++ src/contratos/frmContratos.java
@@ -14,12 +14,9 @@ import servicios.servicioExtra;
  *
  * @author mr117
  */
-public class frmContratos extends javax.swing.JFrame {
+public class frmContratos extends javax.swing.JInternalFrame {
     private ControladorContrato controlador;
-    
-    
-    private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(frmContratos.class.getName());
-
+   
     /**
      * Creates new form Contratos
      */
@@ -66,7 +63,7 @@ public class frmContratos extends javax.swing.JFrame {
         txtFechaInicial = new javax.swing.JTextField();
         txtFechaFinal = new javax.swing.JTextField();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         lblTitulo.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         lblTitulo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -505,27 +502,6 @@ public class frmContratos extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
-            logger.log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new frmContratos().setVisible(true));
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnActivar;
